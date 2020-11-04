0962393054f66441313bac03330225fa54781118
Dawid Pereira <pereiradawid@outlook.com>
2020-11-04 21:43:42 +0100

    Added base recognition of images from selected folder

--- ImageRecognition/MainWindow.xaml.cs
+++ ImageRecognition/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Forms;
+using ImageRecognitionML.Model;
+using Microsoft.Win32;
+using MessageBox = System.Windows.MessageBox;
 
 namespace ImageRecognition
 {
@@ -10,6 +15,27 @@ namespace ImageRecognition
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+
+            if (dialog.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+
+            var directory = new DirectoryInfo(dialog.SelectedPath);
+            var files = directory.GetFiles();
+
+            var input = new ModelInput();
+
+            foreach(var file in files)
+            {
+                input.ImageSource = file.FullName;
+                var result = ConsumeModel.Predict(input);
+
+                MessageBox.Show("You selected: " + result.Prediction);
+            }
         }
     }
 }
