bb19b10a783a451589b3dfa6f418584a3df57c67
Dawid Pereira <pereiradawid@outlook.com>
2020-11-05 18:30:24 +0100

    Searching images by selected prediction

---
+++ ImageRecognition/ImageDetails.cs
@@ -0,0 +1,16 @@
+﻿namespace ImageRecognition
+{
+    public class ImageDetails
+    {
+        public ImageDetails(string name, string fullName, int index)
+        {
+            Name = name;
+            FullName = fullName;
+            Index = index;
+        }
+        public string Name { get; private set; }
+        public string FullName { get; private set; }
+
+        public int Index { get; private set; }
+    }
+}
--- ImageRecognition/MainWindow.xaml.cs
+++ ImageRecognition/MainWindow.xaml.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using ImageRecognitionML.Model;
+using MessageBox = System.Windows.MessageBox;
 
 namespace ImageRecognition
 {
@@ -19,7 +18,14 @@ namespace ImageRecognition
     {
 
         private int _imageIndex = 0;
-        private readonly List<string> _imagesSources = new List<string>();
+        private string _selectedPrediction;
+        private readonly List<ImageDetails> _imagesSources = new List<ImageDetails>();
+        private readonly List<string> _predictions = new List<string>
+        {
+            "Human",
+            "Mountains"
+        };
+
 
         public MainWindow()
         {
@@ -28,6 +34,12 @@ namespace ImageRecognition
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            ImagesList.Items.Clear();
+            if (_selectedPrediction == null)
+            {
+                MessageBox.Show("Select your prediction, before you pick folder.");
+                return;
+            }
             var dialog = new FolderBrowserDialog();
 
             if (dialog.ShowDialog() != System.Windows.Forms.DialogResult.OK)
@@ -40,19 +52,24 @@ namespace ImageRecognition
             var files = directory.GetFiles();
 
             var input = new ModelInput();
+            var index = 0;
 
             foreach(var file in files.Where(x => ".jpg|.jpeg|.png".Contains(x.Extension.ToLower())))
             {
                 input.ImageSource = file.FullName;
                 var result = ConsumeModel.Predict(input);
+                if(!string.Equals(result.Prediction, _selectedPrediction, StringComparison.CurrentCultureIgnoreCase))
+                    continue;
+
                 var accuracy = result.Score.Max();
                 if (accuracy < 0.8)
                     continue;
-                //MessageBox.Show($"You selected: {file.Name} | Prediction: {result.Prediction} | Accuracy: {accuracy}");
-                _imagesSources.Add(file.FullName);
+                _imagesSources.Add(new ImageDetails(file.Name, file.FullName, index));
+                index++;
             }
-
-            GetImage();
+            _imagesSources.ForEach(x => ImagesList.Items.Add(x));
+            ImagesList.DisplayMemberPath = "Name";
+            LoadImage();
         }
 
         private void PreviousBTN_Click(object sender, RoutedEventArgs e)
@@ -65,7 +82,7 @@ namespace ImageRecognition
             }
 
             Console.WriteLine($"ImageIndex: {_imageIndex} | ImagesCount: {_imagesSources.Count} ");
-            GetImage();
+            LoadImage();
         }
 
         private void NextBTN_Click(object sender, RoutedEventArgs e)
@@ -79,17 +96,35 @@ namespace ImageRecognition
             }
 
             Console.WriteLine($"ImageIndex: {_imageIndex} | ImagesCount: {_imagesSources.Count} ");
-            GetImage();
+            LoadImage();
         }
 
-        private void GetImage()
+        private void LoadImage()
         {
-            var uri = new Uri(_imagesSources[_imageIndex], UriKind.Absolute);
+            var uri = !_imagesSources.Any()
+                ? new Uri("C:\\Users\\Dawid\\RiderProjects\\image-recognition\\ImageRecognition\\Files\\404_file_not_found.jpg", UriKind.Absolute)
+                : new Uri(_imagesSources[_imageIndex].FullName, UriKind.Absolute);
             var source = new BitmapImage();
             source.BeginInit();
             source.UriSource = uri;
             source.EndInit();
             MainImage.Source = source;
+        }
+
+        private void ImagesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!(e.AddedItems[0] is ImageDetails imageDetails))
+                return;
+            _imageIndex = imageDetails.Index;
+            LoadImage();
+
+        }
+
+        private void Predictions_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!(e.AddedItems[0] is ListBoxItem prediction))
+                return;
+            _selectedPrediction = prediction.Content.ToString();
         }
     }
 }
